7a772d6880e322884d4e701d7911def2a3a61185
Falko <falko@polymap.de>
2015-11-05 19:37:05 +0100

    #17: Suche zurücksetzen (http://github.com/fb71/polymap4-alkis/issues/issue/17)
      - Zurücksetzen einzelner Felder durch Löschen mit Tastatur unterbinden
      - Zähler und Flur als Zahlen validieren

--- plugins/org.polymap.alkis/src/org/polymap/alkis/ui/FlurstueckFilterPage.java
+++ plugins/org.polymap.alkis/src/org/polymap/alkis/ui/FlurstueckFilterPage.java
@@ -25,16 +25,21 @@ import static org.polymap.alkis.model.fulltext.FlurstueckTransformer.GMK;
 import static org.polymap.alkis.model.fulltext.FlurstueckTransformer.LAGE;
 import static org.polymap.alkis.model.fulltext.FlurstueckTransformer.NENNER;
 import static org.polymap.alkis.model.fulltext.FlurstueckTransformer.ZAEHLER;
+import static org.polymap.rhei.field.Validators.AND;
 
 import org.opengis.filter.Filter;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.rap.rwt.RWT;
 
 import org.polymap.core.ui.ColumnLayoutFactory;
 
+import org.polymap.rhei.field.NullValidator;
+import org.polymap.rhei.field.NumberValidator;
 import org.polymap.rhei.filter.DefaultFilterPage;
 import org.polymap.rhei.filter.IFilterPage2;
 import org.polymap.rhei.filter.IFilterPageSite;
@@ -56,6 +61,33 @@ public abstract class FlurstueckFilterPage
     protected IFilterPageSite           pageSite;
     
     protected String                    queryString;
+
+    
+    /**
+     * 
+     */
+    class NotClearedValidator
+            extends NullValidator {
+
+        private String              previous;
+        
+        @Override
+        public String validate( Object fieldValue ) {
+            if (fieldValue == null && previous != null && previous.length() == 1) {
+                previous = (String)fieldValue;
+                return "Benutzen Sie ZURÜCKSETZEN, um Felder zu leeren";                
+            }
+            previous = (String)fieldValue;
+            
+            if (fieldValue != null) {
+                String s = (String)fieldValue;
+                if (s == null || s.length() == 0 || StringUtils.containsOnly( s, " \t\n\r" )) {
+                    return "Dieses Feld darf nicht leer sein";
+                }
+            }
+            return null;
+        }
+    }
     
     
     @Override
@@ -65,32 +97,58 @@ public abstract class FlurstueckFilterPage
         site.getPageBody().setLayout( ColumnLayoutFactory.defaults().margins( 100, 10 ).spacing( 3 ).columns( 1, 2 ).create() );
         
         PropertyInfo propInfo = AX_Flurstueck.TYPE.zaehler.info();
-        site.newFilterField( ZAEHLER, propInfo.getType() ).label.put( "Zähler" ).create();
+        site.newFilterField( ZAEHLER, propInfo.getType() )
+                .label.put( "Zähler" )
+                .validator.put( AND( new NotClearedValidator(), new NumberValidator( Integer.class, RWT.getLocale() ) ) )
+                .create();
 
         propInfo = AX_Flurstueck.TYPE.nenner.info();
-        site.newFilterField( NENNER, propInfo.getType() ).label.put( "Nenner" ).create();
+        site.newFilterField( NENNER, propInfo.getType() )
+                .label.put( "Nenner" )
+                .validator.put( new NotClearedValidator() ).create();
 
-        site.newFilterField( FLUR, Integer.class ).label.put( "Flur" ).create();
-        site.newFilterField( LAGE, String.class ).label.put( "Lage*" )
-                .tooltip.put( "Zeichenkette, die in Lagebezeichnung mit/ohne Hausnummer oder verschlüsselte Lage enthalten ist" ).create();
-
-        site.newFilterField( GMK, String.class ).label.put( "Gemarkung" ).create();
-        site.newFilterField( GMD, String.class ).label.put( "Gemeinde" ).create();        
-
-        site.newFilterField( BB, String.class ).label.put( "Buchungsblatt*" )
-                .tooltip.put( "Zeichenkette, die in Buchungsblattnummer oder -Kennzeichen enthalten ist" ).create();        
-
-        site.newFilterField( BESITZER_NAME, String.class ).label.put( "Besitzer-Name*" )
-                .tooltip.put( "Zeichenkette, die im Namen oder Firma des Besitzers enthalten ist" ).create();
+        site.newFilterField( FLUR, Integer.class )
+                .label.put( "Flur" )
+                .validator.put( AND( new NotClearedValidator(), new NumberValidator( Integer.class, RWT.getLocale() ) ) )
+                .create();
         
-        site.newFilterField( BESITZER_VORNAME, String.class ).label.put( "Besitzer-Vorname*" )
-                .tooltip.put( "Zeichenkette, die im Vornamen des Besitzers enthalten ist" ).create();        
+        site.newFilterField( LAGE, String.class )
+                .label.put( "Lage*" )
+                .tooltip.put( "Zeichenkette, die in Lagebezeichnung mit/ohne Hausnummer oder verschlüsselte Lage enthalten ist" )
+                .validator.put( new NotClearedValidator() ).create();
+
+        site.newFilterField( GMK, String.class )
+                .label.put( "Gemarkung" )
+                .validator.put( new NotClearedValidator() ).create();
         
-        site.newFilterField( BESITZER_GEBURTSNAME, String.class ).label.put( "Geburtsname*" )
-                .tooltip.put( "Zeichenkette, die im Vornamen des Besitzers enthalten ist" ).create();
+        site.newFilterField( GMD, String.class )
+                .label.put( "Gemeinde" )
+                .validator.put( new NotClearedValidator() ).create();        
+
+        site.newFilterField( BB, String.class )
+                .label.put( "Buchungsblatt*" )
+                .tooltip.put( "Zeichenkette, die in Buchungsblattnummer oder -Kennzeichen enthalten ist" )
+                .validator.put( new NotClearedValidator() ).create();        
+
+        site.newFilterField( BESITZER_NAME, String.class )
+                .label.put( "Besitzer-Name*" )
+                .tooltip.put( "Zeichenkette, die im Namen oder Firma des Besitzers enthalten ist" )
+                .validator.put( new NotClearedValidator() ).create();
         
-        site.newFilterField( BESITZER_ANSCHRIFT, String.class ).label.put( "Besitzer-Anschrift*" )        
-                .tooltip.put( "Zeichenkette, die in Ort, PLZ, Strasse oder Hausnummer enthalten ist" ).create();
+        site.newFilterField( BESITZER_VORNAME, String.class )
+                .label.put( "Besitzer-Vorname*" )
+                .tooltip.put( "Zeichenkette, die im Vornamen des Besitzers enthalten ist" )
+                .validator.put( new NotClearedValidator() ).create();        
+        
+        site.newFilterField( BESITZER_GEBURTSNAME, String.class )
+                .label.put( "Geburtsname*" )
+                .tooltip.put( "Zeichenkette, die im Vornamen des Besitzers enthalten ist" )
+                .validator.put( new NotClearedValidator() ).create();
+        
+        site.newFilterField( BESITZER_ANSCHRIFT, String.class )
+                .label.put( "Besitzer-Anschrift*" )        
+                .tooltip.put( "Zeichenkette, die in Ort, PLZ, Strasse oder Hausnummer enthalten ist" )
+                .validator.put( new NotClearedValidator() ).create();
     }
 
     
@@ -116,16 +174,19 @@ public abstract class FlurstueckFilterPage
     protected void appendQueryField( String field, StringBuilder result, boolean isLike ) {
         Object value = pageSite.getFieldValue( field );
         if (value != null) {
-            String stringValue = value.toString();
-            if (stringValue.length() > 0) {
+            String expression = value.toString();
+            if (expression.length() > 0) {
                 if (result.length() > 0) {
                     result.append( " AND " );
                 }
                 
-                if (!stringValue.contains( "*" )) {
-                    stringValue = "\"" + stringValue + "\"";
+                if (isLike) {
+                    expression = "\"*" + expression + "*\"";
                 }
-                result.append( field ).append( ":").append( stringValue );
+                else if (!expression.contains( "*" )) {
+                    expression = "\"" + expression + "\"";
+                }
+                result.append( field ).append( ":" ).append( expression );
                 
 //                if (stringValue.contains( "*" )) {
 //                    result.append( field ).append( ":").append( stringValue );
